1df5744d79b4c6c05ed5d3c6b3d60f00dc0294d4
Yukio Oobuchi <Yukio.Oobuchi@sony.com>
2023-03-15 18:12:02 +0900

    Separate dev and release

--- canvas/test02/Gulpfile.js
+++ canvas/test02/Gulpfile.js
@@ -11,12 +11,15 @@ const gulpStripImportExport = require('gulp-strip-import-export');
 const gulpUglify = require('gulp-uglify');
 
 
+const jsSources = 'src/js/*.js';
+const cssSources = 'src/css/*.css';
+
 const clean = () => {
-  return gulp.src('release/', {allowEmpty:true})
+  return gulp.src('dist/', {allowEmpty:true})
     .pipe(gulpClean());
 };
 
-const javascript = () => {
+const jsRelease = () => {
   return gulp.src('src/js/*.js')
     .pipe(gulpEslint({useEslintrc: true}))
     .pipe(gulpEslint.format())
@@ -24,25 +27,60 @@ const javascript = () => {
     .pipe(gulpConcat('test02.js'))
     .pipe(gulpStripImportExport())
     .pipe(gulpBabel({presets: ['@babel/env']}))
-//    .pipe(gulpUglify())
+    .pipe(gulpUglify())
     .pipe(gulpRename({extname: '.min.js'}))
-    .pipe(gulp.dest('release/'));
+    .pipe(gulp.dest('dist/release/'));
+};
+
+const jsDev = () => {
+  return gulp.src('src/js/*.js')
+    .pipe(gulpEslint({useEslintrc: true}))
+    .pipe(gulpEslint.format())
+    .pipe(gulpEslint.failAfterError())
+    .pipe(gulpConcat('test02.js'))
+    .pipe(gulpStripImportExport())
+    .pipe(gulpBabel({presets: ['@babel/env']}))
+    .pipe(gulpRename({extname: '.js'}))
+    .pipe(gulp.dest('dist/dev/'));
   
 };
 
-const html = () => {
-  return gulp.src('src/html/index.html')
-    .pipe(gulp.dest('release/'));
-};
-
-const css = () => {
+const cssRelease = () => {
   return gulp.src('src/css/*.css')
     .pipe(gulpCSSlint('.csslintrc.json'))
     .pipe(gulpConcat('test02.css'))
-//    .pipe(gulpCleanCSS())
+    .pipe(gulpCleanCSS())
     .pipe(gulpRename({extname: '.min.css'}))
-    .pipe(gulp.dest('release/'));
+    .pipe(gulp.dest('dist/release/'));
 };
 
-exports.default = gulp.series(clean, javascript, html, css);
+const cssDev = () => {
+  return gulp.src('src/css/*.css')
+    .pipe(gulpCSSlint('.csslintrc.json'))
+    .pipe(gulpConcat('test02.css'))
+    .pipe(gulpRename({extname: '.css'}))
+    .pipe(gulp.dest('dist/dev/'));
+};
+
+const htmlRelease = () => {
+  return gulp.src('src/html/release/index.html')
+    .pipe(gulp.dest('dist/release/'));
+};
+
+const htmlDev = () => {
+  return gulp.src('src/html/dev/index.html')
+    .pipe(gulp.dest('dist/dev/'));
+};
+
+exports.default = gulp.series(
+  clean,
+  gulp.parallel(
+    jsRelease,
+    cssRelease,
+    htmlRelease,
+    jsDev,
+    cssDev,
+    htmlDev,
+  ));
+
 
