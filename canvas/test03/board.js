64649f783f1248f4ca103d4c5df9d4e3c2775b41
Yukio Obuchi <buchio@buchio.com>
2023-04-13 06:59:02 +0900

    Start using memCanvas.

--- canvas/test03/board.js
+++ canvas/test03/board.js
@@ -17,96 +17,126 @@ class Board {
     this.message = null;
   }
 
-  init(textCanvas, maskCanvas) {
-    this.textCanvas = textCanvas;
-    this.maskCanvas = maskCanvas;
+  init(div) {
+    this.div = div;
+    this.canvas = null;
     this.scrollPosition = null;
+    this.currentDate = -1;
+    this.currentMessage = null;
   }
   
   anim() {
-    if ( this.message == null ) {
+
+
+    const now = Date.now();
+    if (now - this.currentDate < 500) {
       return;
     }
-    const textCanvas = this.textCanvas;
-    const maskCanvas = this.maskCanvas;
-
-    const th = Math.round(this.settings.FONT_SIZE * this.settings.RATE_MARGIN);
-    const tr = textCanvas.clientWidth / textCanvas.clientHeight;
-    const tw = Math.round(th * tr);
-    const mw = tw * this.settings.MASK_SIZE_RATIO;
-    const mh = th * this.settings.MASK_SIZE_RATIO;
-
-    if ( textCanvas.width != tw ||
-         textCanvas.height != th ||
-         maskCanvas.width != mw ||
-         maskCanvas.height != mh ) {
-      textCanvas.width = tw;
-      textCanvas.height = th;
-      maskCanvas.width = mw;
-      maskCanvas.height = mh;
-      this.scrollPosition = null;
-    }
-    const textCtx = textCanvas.getContext("2d");
+    this.currentDate = now;
     
-    const now = Date.now();
-    textCtx.save();
-    textCtx.font = `${this.settings.FONT_SIZE}px "Lucida Console", Courier, monospace`;
-    textCtx.textAlign = "left";
-    textCtx.textBaseline = "top";
-    const text = textCtx.measureText(this.message);
-    const scrollWidth = text.width+tw;
-    const speed = 1000 / this.settings.FONT_SIZE / this.settings.SCROLL_SPEED;
-    const scrollPosition = Math.floor(-((now/speed) % scrollWidth)+tw);
+    const rect = this.div.getBoundingClientRect();   
+    this.div.style.left = (rect.left - 10) + 'px';
+    console.log(this.currentDate, this.div.x, this.div.style.left, this.div.clientWidth, this.div.clientHeight)
+    
+    if ( this.message != this.currentMessage ) {
+      console.log(`Message ${this.message}`);
+      this.currentMessage = this.message;
 
-    if (scrollPosition != this.scrollPosition) {
+      delete this.canvas;
+      const txtCanvas = document.createElement("canvas");
 
-      // Draw text
-      textCtx.clearRect(0, 0, tw, th);
-      textCtx.save();
-      textCtx.filter = "contrast(1)";
-      textCtx.fillStyle = this.settings.FONT_COLOR;
-      textCtx.fillText(this.message,
-                       scrollPosition,
-                       Math.round(th*this.settings.RATE_POSITION));
-      textCtx.restore();
-      
-      if (this.scrollPosition == null) {
-        console.log('Draw mask!');
-        // Draw mask
-        const maskCtx = maskCanvas.getContext("2d");
-        const d = mh / textCanvas.height;
-        const r = d*this.settings.MASK_LED_RADIUS_RATE;
-        const a = Math.PI * 2;
-
-        maskCtx.clearRect(0, 0, mw, mh);
-        maskCtx.save();
-        for (let x = 0; x < mw; x+=d ) {
-          const ix = x+d/2;
-          for (let y = 0; y < mh; y+=d ) {
-            const iy = y+d/2;
-            maskCtx.fillStyle = "#111f";
-            maskCtx.fillRect(x, y, d, d);
-            maskCtx.fillStyle = "#fff";
-            maskCtx.globalCompositeOperation = "destination-out";
-            maskCtx.beginPath();
-            maskCtx.arc(ix, iy, r, 0, a, true);
-            maskCtx.fill();
-            maskCtx.fillStyle = "#0000";
-            maskCtx.globalCompositeOperation = "source-over";
-            maskCtx.beginPath();
-            maskCtx.arc(ix, iy, r, 0, a, true);
-            maskCtx.fill();
-          }
-        }
-        maskCtx.restore();
+      { // Measure text size
+        const txtCtx = txtCanvas.getContext('2d');
+        txtCtx.save();
+        txtCtx.font = `${this.settings.FONT_SIZE}px "Lucida Console", Courier, monospace`;
+        txtCtx.textAlign = "left";
+        txtCtx.textBaseline = "top";
+        const text = txtCtx.measureText(this.message);
+        console.log(text);
+        txtCtx.restore();
       }
-      this.scrollPosition = scrollPosition;
     }
-    textCtx.restore();
+    
+//    const textCanvas = this.textCanvas;
+//    const maskCanvas = this.maskCanvas;
+//
+//    const th = Math.round(this.settings.FONT_SIZE * this.settings.RATE_MARGIN);
+//    const tr = textCanvas.clientWidth / textCanvas.clientHeight;
+//    const tw = Math.round(th * tr);
+//    const mw = tw * this.settings.MASK_SIZE_RATIO;
+//    const mh = th * this.settings.MASK_SIZE_RATIO;
+//
+//    if ( textCanvas.width != tw ||
+//         textCanvas.height != th ||
+//         maskCanvas.width != mw ||
+//         maskCanvas.height != mh ) {
+//      textCanvas.width = tw;
+//      textCanvas.height = th;
+//      maskCanvas.width = mw;
+//      maskCanvas.height = mh;
+//      this.scrollPosition = null;
+//    }
+//    const textCtx = textCanvas.getContext("2d");
+//    
+//    const now = Date.now();
+//    textCtx.save();
+//    textCtx.font = `${this.settings.FONT_SIZE}px "Lucida Console", Courier, monospace`;
+//    textCtx.textAlign = "left";
+//    textCtx.textBaseline = "top";
+//    const text = textCtx.measureText(this.message);
+//    const scrollWidth = text.width+tw;
+//    const speed = 1000 / this.settings.FONT_SIZE / this.settings.SCROLL_SPEED;
+//    const scrollPosition = Math.floor(-((now/speed) % scrollWidth)+tw);
+//
+//    if (scrollPosition != this.scrollPosition) {
+//
+//      // Draw text
+//      textCtx.clearRect(0, 0, tw, th);
+//      textCtx.save();
+//      textCtx.filter = "contrast(1)";
+//      textCtx.fillStyle = this.settings.FONT_COLOR;
+//      textCtx.fillText(this.message,
+//                       scrollPosition,
+//                       Math.round(th*this.settings.RATE_POSITION));
+//      textCtx.restore();
+//      
+//      if (this.scrollPosition == null) {
+//        console.log('Draw mask!');
+//        // Draw mask
+//        const maskCtx = maskCanvas.getContext("2d");
+//        const d = mh / textCanvas.height;
+//        const r = d*this.settings.MASK_LED_RADIUS_RATE;
+//        const a = Math.PI * 2;
+//
+//        maskCtx.clearRect(0, 0, mw, mh);
+//        maskCtx.save();
+//        for (let x = 0; x < mw; x+=d ) {
+//          const ix = x+d/2;
+//          for (let y = 0; y < mh; y+=d ) {
+//            const iy = y+d/2;
+//            maskCtx.fillStyle = "#111f";
+//            maskCtx.fillRect(x, y, d, d);
+//            maskCtx.fillStyle = "#fff";
+//            maskCtx.globalCompositeOperation = "destination-out";
+//            maskCtx.beginPath();
+//            maskCtx.arc(ix, iy, r, 0, a, true);
+//            maskCtx.fill();
+//            maskCtx.fillStyle = "#0000";
+//            maskCtx.globalCompositeOperation = "source-over";
+//            maskCtx.beginPath();
+//            maskCtx.arc(ix, iy, r, 0, a, true);
+//            maskCtx.fill();
+//          }
+//        }
+//        maskCtx.restore();
+//      }
+//      this.scrollPosition = scrollPosition;
+//    }
+//    textCtx.restore();
   }
 };
 
-const board = new Board({
+const board1 = new Board({
   FONT_SIZE: 16,
   SCROLL_SPEED: .2
 });
@@ -119,11 +149,12 @@ const board2 = new Board({
 });
 
 function init() {
-  board.init(document.getElementById('text'),
-             document.getElementById('mask'));
-  board2.init(document.getElementById('text2'),
-             document.getElementById('mask2'));
-  board2.message = "メッセージを表示します。";
+  board1.init(document.getElementById('board1'));
+  board2.init(document.getElementById('board2'));
+  //           document.getElementById('mask'));
+  //board2.init(document.getElementById('text2'),
+  //           document.getElementById('mask2'));
+  board1.message = "メッセージを表示します。";
   anim();
 }
 
@@ -134,10 +165,10 @@ const dateTimeFormat = new Intl.DateTimeFormat(
   });
 
 function anim() {
-
   const date = new Date();
-  board.message = dateTimeFormat.format(date);
-  board.anim();
+  board2.message = dateTimeFormat.format(date);
+  delete date;
+  board1.anim();
   board2.anim();
   window.requestAnimationFrame(anim);
 }
@@ -145,4 +176,4 @@ function anim() {
 function onLoad() {
   init();
 }
-    
+   
